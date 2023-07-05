a58694af2884927779e9b44c9aa261c9558530b9
Victor Ajayi <ovajayi@gmail.com>
2023-07-05 19:38:32 +0100

    enum, inheritance, interfaces

--- vite-project/src/components/Checking.tsx
+++ vite-project/src/components/Checking.tsx
@@ -15,3 +15,61 @@ const stuff: (number | string)[] = [1, 2, 3, "James"]; //Union types and Arrays
 const coord: (Point | Loc)[] = [];
 
 let userMood: "Happy" | "Sad";
+
+enum ArrowKeys {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+// To avoid typescript adding more code to javascript compilation
+
+const enum ArrowKeys2 {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+const arrowUp = {
+  number: 1,
+  status: ArrowKeys.UP,
+};
+
+const arrowDown = {
+  number: 1,
+  status: ArrowKeys2.DOWN,
+};
+
+// interfaces
+interface POINT {
+  x: number;
+  y: number;
+}
+
+// Multiple Inheritance
+
+interface Human {
+  name: string;
+}
+
+interface Work {
+  level: string;
+  specialty: string[];
+}
+
+interface HumanWork extends Human, Work {
+  native: string;
+  age: number;
+}
+
+const Engineer: HumanWork = {
+  native: "English",
+  level: "senior",
+  age: 20,
+  specialty: ["JS", "React Js"],
+  name: "Victor James",
+};
+
+// Interfaces can only describe object shapes, type alias can describe any sort of types- object, function, union
